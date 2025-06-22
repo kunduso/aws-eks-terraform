3621ad82514210ceb51a155f9a7959bdf0ba1b26
sourav kundu <kunduso@amazon.com>
2025-06-21 22:21:12 -0500

    update provider version

--- infra/provider.tf
+++ infra/provider.tf
@@ -2,21 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.9.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5.1"
-    }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0.4"
-    }
-
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
+      version = "5.97.0"
     }
   }
 }
@@ -27,7 +13,7 @@ provider "aws" {
   secret_key = var.secret_key
   default_tags {
     tags = {
-      Source = "https://github.com/kunduso-aws/aws-eks-terraform"
+      Source = "https://github.com/kunduso/aws-eks-terraform"
     }
   }
 }
