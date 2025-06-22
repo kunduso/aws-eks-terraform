436fae9c4e7c281db55a6c9123e076bce065e9da
sourav kundu <kunduso@amazon.com>
2025-06-21 22:16:19 -0500

    removed duplicate unnecessary configurations

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "kunduso-terraform-remote-bucket"
-    encrypt = true
-    key     = "tf/aws-eks-terraform/terraform.tfstate"
-    region  = "us-east-2"
-  }
-}
