77ead86a8543df9b604db5f9d250dc7d01396e20
sourav kundu <kunduso@amazon.com>
2025-06-22 10:14:14 -0500

    updated policy to enable eks nodes to pull

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,11 +1,9 @@
 resource "aws_ecr_repository" "image_repo" {
   name                 = var.name
   image_tag_mutability = "IMMUTABLE"
-
   image_scanning_configuration {
     scan_on_push = true
   }
-
   tags = {
     Name = "${var.name}"
   }
@@ -15,6 +13,7 @@ resource "aws_ecr_repository" "image_repo" {
 resource "aws_ecr_repository_policy" "repo_policy" {
   repository = aws_ecr_repository.image_repo.name
 
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -22,7 +21,7 @@ resource "aws_ecr_repository_policy" "repo_policy" {
         Sid    = "AllowPullFromEKSNodes",
         Effect = "Allow",
         Principal = {
-          AWS = "*" #aws_iam_role.eks_nodes.arn
+          AWS = aws_iam_role.eks_nodes.arn
         },
         Action = [
           "ecr:BatchGetImage",
