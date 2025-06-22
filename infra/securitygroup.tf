e89d6c8c907f6f5099f321eff111a4a176a08ba8
sourav kundu <kunduso@amazon.com>
2025-06-22 14:23:37 -0500

    update sg and rules for #4

--- infra/securitygroup.tf
+++ infra/securitygroup.tf
@@ -7,15 +7,6 @@ resource "aws_security_group" "eks_cluster" {
   }
 }
 
-resource "aws_security_group" "eks_nodes" {
-  name        = "${var.name}-eks-nodes-sg"
-  description = "Security group for EKS worker nodes"
-  vpc_id      = module.vpc.vpc.id
-  tags = {
-    Name = "${var.name}-eks-nodes-sg"
-  }
-}
-
 # Cluster egress rules
 resource "aws_security_group_rule" "cluster_https_egress" {
   description       = "HTTPS egress for cluster"
@@ -27,76 +18,3 @@ resource "aws_security_group_rule" "cluster_https_egress" {
   type              = "egress"
 }
 
-# Node egress rules
-resource "aws_security_group_rule" "nodes_https_egress" {
-  description       = "HTTPS egress for ECR and AWS APIs"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.eks_nodes.id
-  type              = "egress"
-}
-
-resource "aws_security_group_rule" "nodes_http_egress" {
-  description       = "HTTP egress for package updates"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.eks_nodes.id
-  type              = "egress"
-}
-
-resource "aws_security_group_rule" "nodes_dns_egress" {
-  description       = "DNS egress"
-  from_port         = 53
-  to_port           = 53
-  protocol          = "udp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.eks_nodes.id
-  type              = "egress"
-}
-
-resource "aws_security_group_rule" "nodes_ntp_egress" {
-  description       = "NTP egress"
-  from_port         = 123
-  to_port           = 123
-  protocol          = "udp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.eks_nodes.id
-  type              = "egress"
-}
-
-# Allow the cluster to communicate with the worker nodes
-resource "aws_security_group_rule" "cluster_to_nodes" {
-  description              = "Allow cluster to communicate with worker nodes"
-  from_port                = 0
-  protocol                 = "-1"
-  security_group_id        = aws_security_group.eks_nodes.id
-  source_security_group_id = aws_security_group.eks_cluster.id
-  to_port                  = 65535
-  type                     = "ingress"
-}
-
-# Allow the worker nodes to communicate with the cluster
-resource "aws_security_group_rule" "nodes_to_cluster" {
-  description              = "Allow worker nodes to communicate with the cluster API Server"
-  from_port                = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks_cluster.id
-  source_security_group_id = aws_security_group.eks_nodes.id
-  to_port                  = 443
-  type                     = "ingress"
-}
-
-# Allow worker nodes to communicate with each other
-resource "aws_security_group_rule" "nodes_internal" {
-  description              = "Allow worker nodes to communicate with each other"
-  from_port                = 0
-  protocol                 = "-1"
-  security_group_id        = aws_security_group.eks_nodes.id
-  source_security_group_id = aws_security_group.eks_nodes.id
-  to_port                  = 65535
-  type                     = "ingress"
-}
