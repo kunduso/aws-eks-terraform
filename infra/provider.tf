894e141b147e70331ab092150a6053dc5b467fec
sourav kundu <kunduso@amazon.com>
2025-06-22 16:28:23 -0500

    add missing kubernetes provider

--- infra/provider.tf
+++ infra/provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.97.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.37"
+    }
   }
 }
 
@@ -15,5 +19,22 @@ provider "aws" {
     tags = {
       Source = "https://github.com/kunduso/aws-eks-terraform"
     }
+  }
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      aws_eks_cluster.main.name,
+      "--region",
+      var.region
+    ]
   }
 }
