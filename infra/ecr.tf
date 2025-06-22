b143e8f003de27d873979790ff5957db5fc77582
sourav kundu <kunduso@amazon.com>
2025-06-21 22:19:36 -0500

    update ecr repo with custom policy

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,24 +1,35 @@
-resource "aws_ecr_repository" "gene_ai" {
-  name                 = "gene-ai"
+resource "aws_ecr_repository" "image_repo" {
+  name                 = var.name
   image_tag_mutability = "IMMUTABLE"
-  encryption_configuration {
-    encryption_type = "KMS"
-    kms_key         = aws_kms_key.gene_ai.id
-  }
+
   image_scanning_configuration {
     scan_on_push = true
   }
-  lifecycle {
-    ignore_changes = [encryption_configuration]
+
+  tags = {
+    Name = "${var.name}"
   }
 }
 
-resource "aws_kms_key" "gene_ai" {
-  description             = "KMS key for Gene AI"
-  deletion_window_in_days = 7
-  enable_key_rotation     = true
-}
-resource "aws_kms_alias" "gene_ai" {
-  name          = "alias/gene_ai"
-  target_key_id = aws_kms_key.gene_ai.key_id
+# Add repository policy to allow EKS nodes to pull images
+resource "aws_ecr_repository_policy" "hello_world_policy" {
+  repository = aws_ecr_repository.hello_world.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowPullFromEKSNodes",
+        Effect = "Allow",
+        Principal = {
+          AWS = "*" #aws_iam_role.eks_nodes.arn
+        },
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchCheckLayerAvailability"
+        ]
+      }
+    ]
+  })
 }
