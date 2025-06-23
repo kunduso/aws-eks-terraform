09ff2bdfbfe93c2205c797c4f5b7a40dc897965f
sourav kundu <kunduso@amazon.com>
2025-06-23 12:30:15 -0500

    fix for #2

--- infra/provider.tf
+++ infra/provider.tf
@@ -13,8 +13,6 @@ terraform {
 
 provider "aws" {
   region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
   default_tags {
     tags = {
       Source = "https://github.com/kunduso/aws-eks-terraform"
